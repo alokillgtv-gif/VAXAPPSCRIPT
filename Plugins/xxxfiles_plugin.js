a00276b1f988338afc1d331d0add37291c1f4b4d
alokillgtv <alokillgtv@gmail.com>
2026-07-10 16:13:43 +0700

    update xxxfiles_plugin.js

--- Plugins/xxxfiles_plugin.js
+++ Plugins/xxxfiles_plugin.js
@@ -7,7 +7,7 @@ function getManifest() {
         "id": "xxxfiles",
         "name": "xxxfiles",
         "description": "XXX Hay",
-        "version": "1.0",
+        "version": "1.1",
         "BASEURL": "https://www.xxxfiles.com",
         "iconUrl": "https://www.xxxfiles.com/favicon-32x32.png",
         "isEnabled": true,
