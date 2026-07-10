c922dab4d7e263211667784a0c6683d6efa1d855
alokillgtv <alokillgtv@gmail.com>
2026-07-11 01:35:53 +0700

    update testVideo.js

--- Plugins/testVideo.js
+++ Plugins/testVideo.js
@@ -7,7 +7,7 @@ function getManifest() {
         "id": "testvideo",          
         "name": "Test Embed",
         "description": "Nguồn xem phim Online ổn định",
-        "version": "1.0",             
+        "version": "1.1",             
         "baseUrl": BaseURL,
         "iconUrl": "https://crimescenesolutions.co.za/wp-content/uploads/2026/04/phimhayok-io-fav.jpg", 
         "isEnabled": true,
