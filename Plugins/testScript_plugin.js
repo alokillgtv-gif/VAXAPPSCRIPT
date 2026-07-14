52662fc41d199afb953c9a730b8904bcb7550fb2
alokillgtv <alokillgtv@gmail.com>
2026-07-15 04:51:24 +0700

    update testScript_plugin.js

--- Plugins/testScript_plugin.js
+++ Plugins/testScript_plugin.js
@@ -9,7 +9,7 @@ function getManifest() {
         "id": "testScript",          
         "name": "Phim Chill",
         "description": "Phim online",
-        "version": "3.4",             
+        "version": "3.5",             
         "baseUrl": "https://phimchillhdv.im",
         "iconUrl": "https://raw.githubusercontent.com/alokillgtv-gif/VAXAPPSCRIPT/main/img/motherless_logo.jpgphimchill.ico", 
         "isEnabled": true,
