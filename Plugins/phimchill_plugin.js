19be656c3b8661f819b1f5cf21bb82f56fd7bd3a
alokillgtv <alokillgtv@gmail.com>
2026-07-15 13:12:06 +0700

    update phimchill_plugin.js

--- Plugins/phimchill_plugin.js
+++ Plugins/phimchill_plugin.js
@@ -5,7 +5,7 @@ function getManifest() {
         "id": "phimchill",          
         "name": "Phim Chill",
         "description": "Phim online",
-        "version": "1.4",             
+        "version": "1.5",             
         "baseUrl": "https://phimchillhdv.im",
         "iconUrl": "https://raw.githubusercontent.com/alokillgtv-gif/VAXAPPSCRIPT/main/img/motherless_logo.jpgphimchill.ico", 
         "isEnabled": true,
@@ -393,9 +393,9 @@ function textJS($links) {
     // Sử dụng biến $url từ tham số truyền vào thay vì ghi cứng link
     return `
 LINKVIDEO = ${JSON.stringify($links)};
-SCRIPTURL = "https://rawcdn.githack.com/alokillgtv-gif/VAXAPPSCRIPT/a29c9ff8ab30b1c8ff2b474e0a7b399ea44d5d70/buildVideo.js"; 
+SCRIPTURL = "https://rawcdn.githack.com/alokillgtv-gif/VAXAPPSCRIPT/main/buildVideo.js"; 
 if(LINKVIDEO == "false"){
-	SCRIPTURL = "https://rawcdn.githack.com/alokillgtv-gif/VAXAPPSCRIPT/a29c9ff8ab30b1c8ff2b474e0a7b399ea44d5d70/removeADSVIDEO.js";
+	SCRIPTURL = "https://rawcdn.githack.com/alokillgtv-gif/VAXAPPSCRIPT/main/removeADSVIDEO.js";
 }
 const style = document.createElement('style');
 var customcss = 'body { background: black; overflow: hidden; }body * {background: black;display:none!important}';
