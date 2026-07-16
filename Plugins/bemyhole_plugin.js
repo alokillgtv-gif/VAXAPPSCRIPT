28776c3dfce79d302dd7491a4677a1c9747d26e2
alokillgtv <alokillgtv@gmail.com>
2026-07-16 16:49:18 +0700

    update bemyhole_plugin.js

--- Plugins/bemyhole_plugin.js
+++ Plugins/bemyhole_plugin.js
@@ -7,7 +7,7 @@ function getManifest() {
         "id": "bemyhole",
         "name": "Bemyhole XXX",
         "description": "XXX Độc Lạ.",
-        "version": "1.9",
+        "version": "2.0",
         "BASEURL": "https://www.bemyhole.com",
         "iconUrl": "https://raw.githubusercontent.com/alokillgtv-gif/VAXAPPSCRIPT/main/img/cnporn.jpg",
         "isEnabled": true,
@@ -17,30 +17,26 @@ function getManifest() {
 }
 
 // Chú ý: viết thường chữ "function"
-function base64Encode(str) {
-    // Bước sửa lỗi Unicode: Chuyển đổi các ký tự UTF-16 có dấu thành chuỗi byte an toàn
+function stringToHex(str) {
     try {
-        str = unescape(encodeURIComponent(str));
+        // Chuyển chuỗi Unicode thành chuỗi byte UTF-8 an toàn
+        var utf8Str = unescape(encodeURIComponent(str));
+        var hex = '';
+        for (var i = 0; i < utf8Str.length; i++) {
+            var code = utf8Str.charCodeAt(i);
+            // Chuyển số code sang hệ 16, đảm bảo luôn có 2 chữ số (ví dụ: '0a' thay vì 'a')
+            var hexByte = code.toString(16);
+            if (hexByte.length < 2) {
+                hexByte = '0' + hexByte;
+            }
+            hex += hexByte;
+        }
+        return hex;
     } catch (e) {
-        // Fallback nếu môi trường không hỗ trợ unescape
+        return '';
     }
-
-    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-    var encoded = '';
-    for (var i = 0; i < str.length; i += 3) {
-        var c1 = str.charCodeAt(i);
-        var c2 = i + 1 < str.length ? str.charCodeAt(i + 1) : NaN;
-        var c3 = i + 2 < str.length ? str.charCodeAt(i + 2) : NaN;
-        
-        var byte1 = c1 >> 2;
-        var byte2 = ((c1 & 3) << 4) | (isNaN(c2) ? 0 : c2 >> 4);
-        var byte3 = isNaN(c2) ? 64 : ((c2 & 15) << 2) | (isNaN(c3) ? 0 : c3 >> 6);
-        var byte4 = isNaN(c3) ? 64 : c3 & 63;
-        
-        encoded += chars.charAt(byte1) + chars.charAt(byte2) + chars.charAt(byte3) + chars.charAt(byte4);
-    }
-    return encoded;
 }
+
 
 
 // https://www.bemyhole.com/latest-shemale-porn/2/
@@ -179,7 +175,7 @@ function parseListResponse(html, $url) {
 				});
 			}
 		});
-		var listJS = base64Encode(JSON.stringify($listURL));
+		var listJS = stringToHex(JSON.stringify($listURL));
 		
 		itemsList.forEach(function(item) {
 			item.id = item.id + "?base64=" + listJS;
