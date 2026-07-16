7c35be4c23e5c4b222658468572f2716228d3539
alokillgtv <alokillgtv@gmail.com>
2026-07-16 17:01:08 +0700

    update bemyhole_plugin.js

--- Plugins/bemyhole_plugin.js
+++ Plugins/bemyhole_plugin.js
@@ -7,7 +7,7 @@ function getManifest() {
         "id": "bemyhole",
         "name": "Bemyhole XXX",
         "description": "XXX Độc Lạ.",
-        "version": "2.4",
+        "version": "2.5",
         "BASEURL": "https://www.bemyhole.com",
         "iconUrl": "https://raw.githubusercontent.com/alokillgtv-gif/VAXAPPSCRIPT/main/img/cnporn.jpg",
         "isEnabled": true,
@@ -166,9 +166,9 @@ function parseListResponse(html, $url) {
 			if (href && href.indexOf("http") > -1) {
 				var cleanThumb = src.replace(/&amp;/g, '&');
 				var trimhref = href.replace(BASEURL,"");
-				if(numberC < 6){
+				//if(numberC < 6){
 					$listURL.push(trimhref)
-				}
+				//}
 				
 				itemsList.push({
 					"id": href,
