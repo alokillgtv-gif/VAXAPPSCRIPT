aa1986339c3b8f2d3ac86db859a5e7dd86b548da
alokillgtv <alokillgtv@gmail.com>
2026-07-17 10:59:54 +0700

    update testVideo2.js

--- Plugins/testVideo2.js
+++ Plugins/testVideo2.js
@@ -10,7 +10,7 @@ function getManifest() {
         "id": "testvideo2",          
         "name": "Test Exoplayer",
         "description": "Nguồn xem phim Online ổn định",
-        "version": "1.0",             
+        "version": "1.1",             
         "baseUrl": BaseURL,
         "iconUrl": "https://crimescenesolutions.co.za/wp-content/uploads/2026/04/phimhayok-io-fav.jpg", 
         "isEnabled": true,
