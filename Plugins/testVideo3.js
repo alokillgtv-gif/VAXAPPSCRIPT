d1ebd62b973f0b296139feb69209dd9b11e7b41a
alokillgtv <alokillgtv@gmail.com>
2026-07-18 10:41:15 +0700

    update testVideo3.js

--- Plugins/testVideo3.js
+++ Plugins/testVideo3.js
@@ -141,7 +141,8 @@ function parseDetailResponse(html,url) {
 				return JSON.stringify({
 					"url": videoUrl,
 					"isEmbed": true,
-					"embedRegex": $reg,
+					//"embedRegex": $reg,
+					"embedRegex": "(https?://[^\\s'\"]+\\.(?:m3u8|m3u9|mp4)[^'\"]*)"
 					"headers": {
 						"Referer": refUrl,
 						"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
